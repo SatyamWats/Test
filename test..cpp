671f36275551c151d29d1e6760f456f67168c24d
SatyamWats <71392434+SatyamWats@users.noreply.github.com>
2020-10-10 14:58:20 +0530

    Create test..cpp

---
+++ test..cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"\n Enter : ";
+    cin>>n;
+    if(n==10)
+    {
+        cout<<"\n Hello";
+    }
+    n=n+2;
+    if(n==10)
+    {
+        cout<<"\n BYe";
+    }
+    return 0;
+}
